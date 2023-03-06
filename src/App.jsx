d25212e8d1037871b1eb3432a7062f317fd5c676
victor <victor.toledo23@gmail.com>
2023-03-06 15:25:57 -0300

    version nueva

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { MoviesGrid } from "./MoviesGrid"
-import styles from "./App.module.css"
-
+import { MoviesGrid } from "./components/MoviesGrid";
+import styles from "./App.module.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export const App = () => {
   return (
-    <div>
-        <header>
-            <h1 className={styles.title}>Peliculas Estrenos</h1>
-        </header>
-        <main>
-            <MoviesGrid/>
-        </main>
-    </div>
-  )
-}
+    <Router>
+      <header>
+        <h1 className={styles.title}>Peliculas Estrenos</h1>
+      </header>
+      <main>
+        <Switch>
+          <Route path="/movie">Movie</Route>
+          <Route path="">Home</Route>
+        </Switch>
+      </main>
+    </Router>
+  );
+};
