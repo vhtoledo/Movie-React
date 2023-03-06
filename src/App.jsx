73ced752d2b1eff5c3c94995e4df0abc10aedff8
victor <victor.toledo23@gmail.com>
2023-03-06 17:09:02 -0300

    Consumo desde json

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { MoviesGrid } from "./components/MoviesGrid";
 import styles from "./App.module.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { MovieDetails } from "./pages/MovieDetails";
+import { LandingPage } from "./pages/LandingPage";
 
 export const App = () => {
   return (
     <Router>
       <header>
-        <h1 className={styles.title}>Peliculas Estrenos</h1>
+       <Link to="/"><h1 className={styles.title}>Peliculas Estrenos</h1></Link>
       </header>
       <main>
         <Switch>
-          <Route path="/movie">Movie</Route>
-          <Route path="">Home</Route>
+          <Route exact path="/movies/:movieId"><MovieDetails/></Route>
+          <Route exact path=""><LandingPage/></Route>
         </Switch>
       </main>
     </Router>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,20 @@
+import { Link } from "react-router-dom";
 import styles from "./MovieCard.module.css";
 
-export const MovieCard = ({ movies }) => {  
-  const imageUrl = "https://image.tmdb.org/t/p/w300" + movies.poster_path  
-  return <li className= {styles.movieCard} >
-    <img
-        width={230}
-        height={345} 
-        src={imageUrl} 
-        alt={movies.title} 
-        className= {styles.movieImage}/>
-    <div>{movies.title}</div>
+export const MovieCard = ({ movies }) => {
+  const imageUrl = "https://image.tmdb.org/t/p/w300" + movies.poster_path;
+  return (
+    <li className={styles.movieCard}>
+      <Link to={"/movies/" + movies.id}>
+        <img
+          width={230}
+          height={345}
+          src={imageUrl}
+          alt={movies.title}
+          className={styles.movieImage}
+        />
+        <div>{movies.title}</div>
+      </Link>
     </li>
-  
-}
+  );
+};
