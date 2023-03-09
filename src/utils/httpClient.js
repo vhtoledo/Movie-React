e2d4af58ae73c6a4ebdd4c88ddec7c8825918950
victor <victor.toledo23@gmail.com>
2023-03-09 00:51:53 -0300

    Final Proyecto

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,10 +1,11 @@
-const API = "https://api.themoviedb.org/3"
+const API = process.env.REACT_APP_API;
+const API_TOKEN = process.env.REACT_APP_API_TOKEN;
 
 export function get(path) {
     return fetch(API + path, {
         headers: {
             Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Y2Q0ZTQ5NzIxZTlmNDRmOTdhNDkyYWJhYTM1YzVhMyIsInN1YiI6IjY0MDY0YjIzZWY5ZDcyMDBkNzM2N2M4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nAZ0wMSjoFMbLk301tGj1k3WLpJLkRl1sXhC8YbvUlw",
+                "Bearer " + API_TOKEN,
             "Content-Type": "application/json;charset=utf-8",
         },
     }).then((result) => result.json());
